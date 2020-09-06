7d9eac3de34d25622cedd3f83dd053c4fa1cc5be
sy-anwar <syaiful.anwar11111@gmail.com>
2020-09-06 18:24:50 +0700

    add vigenere extended

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 from vigenere_standard import VigenereStandard
+from vigenere_extended import VigenereExtended
 
 if __name__ == "__main__":
-    vs = VigenereStandard()
-    print(vs.encrypt("sonysonysonysony", "thisplaintext", 5))
-    print(vs.decrypt("sony", "LVVQH ZNGFH RVL", 5))
+    # vs = VigenereStandard()
+    # print(vs.encrypt("sony", "thisplaintext", 0))
+    # print(vs.decrypt("sony", "LVVQH ZNGFH RVL", 0))
+    ve = VigenereExtended()
+    print(ve.encrypt("sony", "this plain text", 0))
+    print(ve.decrypt("sony", "ç××ì\x93ßÚÚÜÝ\x8eíØçâ", 0))
+    
