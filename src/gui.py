ea5d87fb061fee674c8aaec0acac138b8f0c5a7f
Fithratulhay Pribadi <pribadi191200@gmail.com>
2020-09-09 00:21:15 +0700

    remove explanation from gui

--- src/gui.py
+++ src/gui.py
@@ -11,17 +11,11 @@ class Gui:
 		self.label_plaintext = Label(self.window, text="Plaintext")
 		self.label_plaintext.grid(column=0, row=0, sticky=W, padx=10)
 
-		self.plaintext = scrolledtext.ScrolledText(self.window, width=40, height=10)
+		self.plaintext = scrolledtext.ScrolledText(self.window, width=65, height=10)
 		self.plaintext.grid(column=0, row=1, columnspan=3, padx=5)
 
 		self.btn_openfile_plaintext = Button(self.window, text="Open File Plaintext")
-		self.btn_openfile_plaintext.grid(column=2, row=0, sticky=E, pady=5, padx=10)
-
-		self.label_plaintext_exlpanation = Label(self.window, text="Plaintext Explanation")
-		self.label_plaintext_exlpanation.grid(column=3, row=0, padx=10)
-
-		self.plaintext_exlpanation = scrolledtext.ScrolledText(self.window, width=15, height=10, state="disabled")
-		self.plaintext_exlpanation.grid(column=3, row=1, padx=5)
+		self.btn_openfile_plaintext.grid(column=2, row=0, sticky=E, pady=5, padx=20)
 
 		self.algorithms = ['Vigenere Cipher', 'Full Vigenere Cipher', 'Auto-key Vigenere Cipher',
 						'Extended Vigenere Cipher', 'Playfair Cipher', 'Super Encription', 
@@ -30,45 +24,46 @@ class Gui:
 		self.label_choose_algorithm = Label(self.window, text='Algorithm :')
 		self.label_choose_algorithm.grid(column=0, row=3, pady=10, padx=10, sticky=SW)
 
-		self.combobox_algorithms = ttk.Combobox(self.window, values=self.algorithms)
+		self.combobox_algorithms = ttk.Combobox(self.window, values=self.algorithms, width=30)
 		self.combobox_algorithms.grid(column=1, row=3, pady=10, sticky=SW)
+		self.combobox_algorithms.current(0)
 		self.combobox_algorithms.bind('<<ComboboxSelected>>', self.handler)
 
 		self.label_key = Label(self.window, text='Key :')
-		self.key = Entry(self.window, width=23)
+		self.label_key.grid(column=0, row=4, sticky=W, padx=10)
+
+		self.key = Entry(self.window, width=40)
+		self.key.grid(column=1, row=4, sticky=W)
 
 		self.label_n_transpotition = Label(self.window, text='n_transpotition :')
-		self.n_transpotition = Entry(self.window, width=23)
+		self.n_transpotition = Entry(self.window, width=40)
 
 		self.label_m = Label(self.window, text='m :')
-		self.m = Entry(self.window, width=23)
+		self.m = Entry(self.window, width=40)
 
 		self.label_b = Label(self.window, text='b :')
-		self.b = Entry(self.window, width=23)
+		self.b = Entry(self.window, width=40)
 		
 		self.label_spaces = Label(self.window, text='Spaces :')
-		self.spaces = Entry(self.window, width=23)
+		self.label_spaces.grid(column=0, row=5, sticky=W, padx=10)
 
-		self.btn_encrypt = Button(self.window, text="Encrypt")
-		self.btn_encrypt.grid(column=2, row=3, sticky='')
+		self.spaces = Entry(self.window, width=40)
+		self.spaces.grid(column=1, row=5, sticky=W)
 
-		self.btn_decrypt = Button(self.window, text="Decrypt")
-		self.btn_decrypt.grid(column=2, row=4, sticky='')
+		self.btn_encrypt = Button(self.window, text="Encrypt", width=15)
+		self.btn_encrypt.grid(column=2, row=3, sticky='E', padx=20)
+
+		self.btn_decrypt = Button(self.window, text="Decrypt", width=15)
+		self.btn_decrypt.grid(column=2, row=4, sticky='E', padx=20)
 
 		self.label_chipertext = Label(self.window, text="Chipertext")
 		self.label_chipertext.grid(column=0, row=7, sticky=W, padx=10, pady=(20, 5))
 
-		self.chipertext = scrolledtext.ScrolledText(self.window, width=40, height=10)
+		self.chipertext = scrolledtext.ScrolledText(self.window, width=65, height=10)
 		self.chipertext.grid(column=0, row=8, columnspan=3, padx=5)
 
 		self.btn_openfile_chipertext = Button(self.window, text="Open File Chipertext")
-		self.btn_openfile_chipertext.grid(column=2, row=7, sticky=E, pady=(20, 5), padx=10)
-
-		self.label_chipertext_exlpanation = Label(self.window, text="Chipertext Explanation")
-		self.label_chipertext_exlpanation.grid(column=3, row=7, padx=10, pady=(20, 5))
-
-		self.chipertext_exlpanation = scrolledtext.ScrolledText(self.window, width=15, height=10, state="disabled")
-		self.chipertext_exlpanation.grid(column=3, row=8, padx=5)
+		self.btn_openfile_chipertext.grid(column=2, row=7, sticky=E, pady=(20, 5), padx=20)
 	
 	def handler(self, event):
 		current = self.combobox_algorithms.current()
@@ -178,11 +173,11 @@ class Gui:
 			self.label_n_transpotition.grid_remove()
 			self.n_transpotition.grid_remove()
 		elif current == 7 : # Hill
-			self.label_spaces.grid(column=0, row=4, sticky=W, padx=10)
-			self.spaces.grid(column=1, row=4, sticky=W)
+			self.label_key.grid(column=0, row=4, sticky=W, padx=10)
+			self.key.grid(column=1, row=4, sticky=W)
 
-			self.label_key.grid_remove()
-			self.key.grid_remove()
+			self.label_spaces.grid(column=0, row=5, sticky=W, padx=10)
+			self.spaces.grid(column=1, row=5, sticky=W)
 
 			self.label_n_transpotition.grid_remove()
 			self.n_transpotition.grid_remove()
